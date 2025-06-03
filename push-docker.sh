18008c26f1ede3970002026790cfe1ef6b35fcb1
OliverStoll <oliverstoll.berlin@gmail.com>
2025-06-03 11:04:46 +0200

    Refactoring

--- push-docker.sh
+++ push-docker.sh
@@ -1,36 +1,36 @@
 # build all dockerfiles
-docker build -t us-west1-docker.pkg.dev/notion-api-sync/my-data/sanitas_scraper:latest -f ./Dockerfile-Sanitas .
-docker build -t us-west1-docker.pkg.dev/notion-api-sync/my-data/diaro_scraper:latest -f ./Dockerfile-Diaro .
-docker build -t us-west1-docker.pkg.dev/notion-api-sync/my-data/budgetbakers_scraper:latest -f ./Dockerfile-Budgetbakers .
-docker build -t us-west1-docker.pkg.dev/notion-api-sync/my-data/ticktick_habits_scraper:latest -f ./Dockerfile-TicktickHabits .
-docker build -t us-west1-docker.pkg.dev/notion-api-sync/my-data/ticktick_tasks_scraper:latest -f ./Dockerfile-TicktickTasks .
-docker build -t us-west1-docker.pkg.dev/notion-api-sync/my-data/ticktick_focus_scraper:latest -f ./Dockerfile-TicktickFocus .
+docker build -t us-west1-docker.pkg.dev/ostoll/my-data/sanitas:latest -f ./Dockerfile-Sanitas .
+docker build -t us-west1-docker.pkg.dev/ostoll/my-data/diaro:latest -f ./Dockerfile-Diaro .
+docker build -t us-west1-docker.pkg.dev/ostoll/my-data/budgetbakers:latest -f ./Dockerfile-Budgetbakers .
+docker build -t us-west1-docker.pkg.dev/ostoll/my-data/ticktick_habits:latest -f ./Dockerfile-TicktickHabits .
+docker build -t us-west1-docker.pkg.dev/ostoll/my-data/ticktick_tasks:latest -f ./Dockerfile-TicktickTasks .
+docker build -t us-west1-docker.pkg.dev/ostoll/my-data/ticktick_focus:latest -f ./Dockerfile-TicktickFocus .
 
 
 # run dockerfiles
-docker run -p 8080:8080 us-west1-docker.pkg.dev/notion-api-sync/my-data/sanitas_scraper:latest
-docker run -p 8080:8080 us-west1-docker.pkg.dev/notion-api-sync/my-data/diaro_scraper:latest
-docker run -p 8080:8080 us-west1-docker.pkg.dev/notion-api-sync/my-data/budgetbakers_scraper:latest
-docker run -p 8080:8080 us-west1-docker.pkg.dev/notion-api-sync/my-data/ticktick_habits_scraper:latest
-docker run -p 8080:8080 us-west1-docker.pkg.dev/notion-api-sync/my-data/ticktick_tasks_scraper:latest
-docker run -p 8080:8080 us-west1-docker.pkg.dev/notion-api-sync/my-data/ticktick_focus_scraper:latest
+docker run -p 8080:8080 us-west1-docker.pkg.dev/ostoll/my-data/sanitas:latest
+docker run -p 8080:8080 us-west1-docker.pkg.dev/ostoll/my-data/diaro:latest
+docker run -p 8080:8080 us-west1-docker.pkg.dev/ostoll/my-data/budgetbakers:latest
+docker run -p 8080:8080 us-west1-docker.pkg.dev/ostoll/my-data/ticktick_habits:latest
+docker run -p 8080:8080 us-west1-docker.pkg.dev/ostoll/my-data/ticktick_tasks:latest
+docker run -p 8080:8080 us-west1-docker.pkg.dev/ostoll/my-data/ticktick_focus:latest
 
 
 # push all dockerfiles
-docker push us-west1-docker.pkg.dev/notion-api-sync/my-data/sanitas_scraper
-docker push us-west1-docker.pkg.dev/notion-api-sync/my-data/diaro_scraper
-docker push us-west1-docker.pkg.dev/notion-api-sync/my-data/budgetbakers_scraper
-docker push us-west1-docker.pkg.dev/notion-api-sync/my-data/ticktick_habits_scraper
-docker push us-west1-docker.pkg.dev/notion-api-sync/my-data/ticktick_tasks_scraper
-docker push us-west1-docker.pkg.dev/notion-api-sync/my-data/ticktick_focus_scraper
+docker push us-west1-docker.pkg.dev/ostoll/my-data/sanitas
+docker push us-west1-docker.pkg.dev/ostoll/my-data/diaro
+docker push us-west1-docker.pkg.dev/ostoll/my-data/budgetbakers
+docker push us-west1-docker.pkg.dev/ostoll/my-data/ticktick_habits
+docker push us-west1-docker.pkg.dev/ostoll/my-data/ticktick_tasks
+docker push us-west1-docker.pkg.dev/ostoll/my-data/ticktick_focus
 
 
 # deploy all dockerfiles
-gcloud run deploy sanitas-scraper  --allow-unauthenticated --image=us-west1-docker.pkg.dev/notion-api-sync/my-data/sanitas_scraper:latest --region=us-west1 --project=notion-api-sync  --memory 1Gi
-gcloud run deploy diaro-scraper  --allow-unauthenticated --image=us-west1-docker.pkg.dev/notion-api-sync/my-data/diaro_scraper:latest --region=us-west1 --project=notion-api-sync --memory 1Gi
-gcloud run deploy budgetbakers-scraper  --allow-unauthenticated --image=us-west1-docker.pkg.dev/notion-api-sync/my-data/budgetbakers_scraper:latest --region=us-west1 --project=notion-api-sync --memory 1Gi
-gcloud run deploy ticktick-habits-scraper  --allow-unauthenticated --image=us-west1-docker.pkg.dev/notion-api-sync/my-data/ticktick_habits_scraper:latest --region=us-west1 --project=notion-api-sync --memory 1Gi
-gcloud run deploy ticktick-tasks-scraper  --allow-unauthenticated --image=us-west1-docker.pkg.dev/notion-api-sync/my-data/ticktick_tasks_scraper:latest --region=us-west1 --project=notion-api-sync --memory 1Gi
-gcloud run deploy ticktick-focus-scraper  --allow-unauthenticated --image=us-west1-docker.pkg.dev/notion-api-sync/my-data/ticktick_focus_scraper:latest --region=us-west1 --project=notion-api-sync --memory 1Gi
+gcloud run deploy sanitas-scraper  --allow-unauthenticated --image=us-west1-docker.pkg.dev/ostoll/my-data/sanitas:latest --region=us-west1 --project=ostoll  --memory 1Gi
+gcloud run deploy diaro-scraper  --allow-unauthenticated --image=us-west1-docker.pkg.dev/ostoll/my-data/diaro:latest --region=us-west1 --project=ostoll --memory 1Gi
+gcloud run deploy budgetbakers-scraper  --allow-unauthenticated --image=us-west1-docker.pkg.dev/ostoll/my-data/budgetbakers:latest --region=us-west1 --project=ostoll --memory 1Gi
+gcloud run deploy ticktick-habits-scraper  --allow-unauthenticated --image=us-west1-docker.pkg.dev/ostoll/my-data/ticktick_habits:latest --region=us-west1 --project=ostoll --memory 1Gi
+gcloud run deploy ticktick-tasks-scraper  --allow-unauthenticated --image=us-west1-docker.pkg.dev/ostoll/my-data/ticktick_tasks:latest --region=us-west1 --project=ostoll --memory 1Gi
+gcloud run deploy ticktick-focus-scraper  --allow-unauthenticated --image=us-west1-docker.pkg.dev/ostoll/my-data/ticktick_focus:latest --region=us-west1 --project=ostoll --memory 1Gi
 
 
