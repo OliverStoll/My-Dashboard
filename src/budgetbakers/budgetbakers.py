e110cf5ceb1a19ced8b2158f61a7d0ab78d06340
OliverStoll <oliverstoll.berlin@gmail.com>
2025-06-03 19:55:50 +0200

    Improve budgetbakers.py

--- src/budgetbakers/budgetbakers.py
+++ src/budgetbakers/budgetbakers.py
@@ -1,94 +1,109 @@
 import os
-from dataclasses import dataclass
-from datetime import datetime, date as Date, timedelta
+from datetime import datetime, date, timedelta
+from pydantic import BaseModel, ConfigDict
 
 from common_utils.web.selenium import DriverAction as Action, SeleniumHandler
 from common_utils.apis.firebase import FirebaseClient
 from common_utils.config import load_dotenv
+from common_utils.logger import create_logger
 
 
-
-@dataclass
-class PaymentRecord:
+class PaymentRecord(BaseModel):
     category: str
     amount: float
     account: str
     recipient: str
     note: str
-    date: Date
+    date: date
+
+    model_config = ConfigDict(from_attributes=True)
 
     def to_dict(self):
-        return {
-            "category": self.category,
-            "amount": self.amount,
-            "account": self.account,
-            "recipient": self.recipient,
-            "note": self.note,
-            "date": self.date.strftime("%d.%m.%Y"),
-        }
+        data = self.model_dump()
+        data["date"] = self.date.strftime("%d.%m.%Y")
+        return data
 
     def __str__(self):
-        return (f"[{self.date.strftime('%d.%m.%Y')}] {self.account}: {self.amount}€ |"
-                f" {self.category} | {self.recipient} | {self.note}")
+        return (
+            f"[{self.date.strftime('%d.%m.%Y')}] {self.account}: {self.amount}€ |"
+            f" {self.category} | {self.recipient} | {self.note}"
+        )
 
 
-@dataclass
-class BalanceRecord:
+class BalanceRecord(BaseModel):
     account: str
     balance: float
-    date: Date
+    date: date
 
     def to_dict(self):
-        return {
-            "account": self.account,
-            "balance": self.balance,
-            "date": self.date.strftime("%d.%m.%Y"),
-        }
+        data = self.model_dump()
+        data["date"] = self.date.strftime("%d.%m.%Y")
+        return data
 
     def __str__(self):
         return f"[{self.date.strftime('%d.%m.%Y')}] {self.account}: {self.balance}€"
 
 
 class BudgetBakersDataScraper:
-    scraper = SeleniumHandler(headless=True, download_driver=True)
+    log = create_logger("BudgetBakers Scraper")
+    debug = bool(os.getenv("DEBUG", False))
+    scraper = SeleniumHandler(headless=not debug, download_driver=True)
     firebase_path = "/DATA/Finanzen/BudgetBakers"
     account_map = {
-        '.': 'Alltag',
-        'Fixkosten': 'Fixkosten',
+        ".": "Alltag",
+        "Fixkosten": "Fixkosten",
     }
 
     def __init__(self) -> None:
         load_dotenv()
         self.firebase = FirebaseClient(realtime_db_url=os.environ["FIREBASE_REALTIME_DB_URL"])
-        self.current_date: Date = datetime.today()
+        self.current_date: date = datetime.today()
 
-    def run(self) -> list[BalanceRecord]:
-        raw_payments, raw_balances = self.get_data()
+    def run(self) -> dict[str, list[PaymentRecord] | list[BalanceRecord]]:
+        data = self.get_data()
+        raw_payments = data.get("records", [])
+        raw_balances = data.get("balances", [])
         payments_data = self.clean_payments_data(raw_payments)
         balances = self.clean_balances_data(raw_balances)
         self.save_payments_to_firestore(payments_data)
         self.save_balances_to_firestore(balances)
-        return balances
+        return {"payments": payments_data, "balances": balances}
 
-    def get_data(self) -> tuple[list[str], list[str]]:
+    def get_data(self) -> dict[str, list[str]]:
         actions = [
-            Action("url", "https://web.budgetbakers.com/login"),
-            Action("sleep", input=4),
-            Action("send_keys", "input[name='email']", os.environ["BUDGETBAKERS_EMAIL"]),
-            Action("send_keys", "input[name='password']", os.environ["BUDGETBAKERS_PASSWORD"]),
-            Action("click", "button[type='submit']"),
-            Action("sleep", input=15),
-            Action("url", "https://web.budgetbakers.com/records"),
-            Action("sleep", input=5),
-            Action("get_texts", "main > div > div:nth-child(2) > div:nth-child(2) > div > div > div", key="records"),
-            Action("sleep", input=2),
-            Action("url", "https://web.budgetbakers.com/accounts"),
-            Action("sleep", input=5),
-            Action("get_texts", "main > div > div:nth-child(2) > div:nth-child(2) > div > div", key="balances"),
+            Action("url", value="https://web.budgetbakers.com/login"),
+            Action("sleep", value=10),
+            Action(
+                "send_keys",
+                css_identifier="input[name='email']",
+                value=os.environ["BUDGETBAKERS_EMAIL"],
+            ),
+            Action(
+                "send_keys",
+                css_identifier="input[name='password']",
+                value=os.environ["BUDGETBAKERS_PASSWORD"],
+            ),
+            Action("click", css_identifier="button[type='submit']"),
+            Action("sleep", value=60),
+            Action("url", value="https://web.budgetbakers.com/records"),
+            Action("sleep", value=10),
+            Action(
+                "get_texts",
+                result_key="records",
+                css_identifier="main > div > div:nth-child(2) > div:nth-child(2) > div > div > div",
+            ),
+            Action("sleep", value=10),
+            Action("url", value="https://web.budgetbakers.com/accounts"),
+            Action("sleep", value=10),
+            Action(
+                "get_texts",
+                result_key="balances",
+                css_identifier="main > div > div:nth-child(2) > div:nth-child(2) > div > div",
+            ),
         ]
-        payments_data = self.scraper.run_actions(actions)['records']
-        balance_data = self.scraper.run_actions(actions)['balances']
-        return payments_data, balance_data
+        results = self.scraper.run_actions(actions)
+        # records, balances
+        return results
 
     def clean_payments_data(self, data_rows: list[str]):
         data_rows = data_rows[4:]
@@ -118,20 +133,21 @@ class BudgetBakersDataScraper:
             balance_records.append(record)
         return balance_records
 
-    def _clean_amount(self, amount_str: str) -> float:
+    @staticmethod
+    def _clean_amount(amount_str: str) -> float:
         amount = amount_str.replace("€", "").replace(",", ".")
         if amount.count(".") > 1:
             amount = amount.replace(".", "", 1)
         return float(amount)
 
-    def _get_current_date(self, date_str: str) -> Date:
+    @staticmethod
+    def _get_current_date(date_str: str) -> date:
         if date_str == "Yesterday":
             return datetime.today().date() - timedelta(days=1)
-        date = datetime.strptime(date_str, "%B %d").date()
+        date_ = datetime.strptime(date_str, "%B %d").date()
         current_year = datetime.today().year
-        year = current_year if date.month <= datetime.today().month else current_year - 1
-        return date.replace(year=year)
-
+        year = current_year if date_.month <= datetime.today().month else current_year - 1
+        return date_.replace(year=year)
 
     def convert_to_record(self, entry_vals: list[str]) -> PaymentRecord:
         amount = entry_vals[4].replace("€", "").replace(",", ".")
@@ -148,19 +164,25 @@ class BudgetBakersDataScraper:
         return record
 
     def save_payments_to_firestore(self, payment_records: list[PaymentRecord]):
+        self.log.info(f"Saving {len(payment_records)} payments to firebase")
         for rec in payment_records:
-            entry_key = f"{rec.amount}|{rec.category}|{rec.recipient}".replace('/', '').replace('.', ',')
-            entry_date = rec.date.strftime('%Y-%m-%d')
+            entry_key = f"{rec.amount}|{rec.category}|{rec.recipient}".replace("/", "").replace(
+                ".", ","
+            )
+            entry_date = rec.date.strftime("%Y-%m-%d")
             entry_ref = f"{self.firebase_path}/records/{rec.account}/{entry_date}/{entry_key}"
             self.firebase.set_entry(ref=entry_ref, data=rec.to_dict())
 
     def save_balances_to_firestore(self, balance_records: list[BalanceRecord]):
+        self.log.info(f"Saving {len(balance_records)} balances to firebase")
         for rec in balance_records:
-            entry_ref = f"{self.firebase_path}/balances/{rec.account}/{rec.date.strftime('%Y-%m-%d')}"
+            entry_ref = (
+                f"{self.firebase_path}/balances/{rec.account}/{rec.date.strftime('%Y-%m-%d')}"
+            )
             self.firebase.set_entry(ref=entry_ref, data=rec.to_dict())
 
 
 if __name__ == "__main__":
-    data = BudgetBakersDataScraper().run()
-    for entry in data:
+    data_ = BudgetBakersDataScraper().run()
+    for entry in data_:
         print(entry)
